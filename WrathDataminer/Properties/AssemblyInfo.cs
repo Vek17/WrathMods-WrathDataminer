88e111534a38696ba472d85a5fdf131ad1ff3228
spacehamster <mail.spacehamster@gmail.com>
2021-02-04 13:25:41 +1100

    Rename project

--- WrathDataminer/Properties/AssemblyInfo.cs
+++ WrathDataminer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VisualAdjustments")]
+[assembly: AssemblyTitle("WrathDataminer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VisualAdjustments")]
+[assembly: AssemblyProduct("WrathDataminer")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
